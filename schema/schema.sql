f98ce9590637d1a4dde030515e4e131e7e8c97f7
onionbulb <phillipvuhuynh@gmail.com>
2025-05-23 17:15:35 -0700

    Updated FK and UNIQUE Constraints to tables

--- schema/schema.sql
+++ schema/schema.sql
@@ -4,7 +4,7 @@ Team Fire 1: Aune Mitchell, Phillip Huynh
 
 Online Marketplace Schema
 */
-
+	
 SET FOREIGN_KEY_CHECKS = 0; -- Disable FK_CHECK to drop existing tables w/o Error 3730
 DROP DATABASE IF EXISTS ecommerce;
 SET FOREIGN_KEY_CHECKS = 1; -- Reenable FK_Check
@@ -19,14 +19,15 @@ CREATE TABLE Product (
     Department VARCHAR(15),
     BuyPrice DECIMAL(7, 2) NOT NULL,
     SellPrice DECIMAL(7, 2) NOT NULL,
-    PRIMARY KEY (ProductID)
+    PRIMARY KEY (ProductID),
+    CONSTRAINT PositivePrice CHECK ((BuyPrice > 0) AND (SellPrice > 0))
 );
 
 CREATE TABLE Inventory (
     ProductID INTEGER,
     Quantity SMALLINT NOT NULL,
     PRIMARY KEY (ProductID),
-    FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
+    CONSTRAINT FK_Inventory_ProductID FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 );
@@ -35,12 +36,12 @@ CREATE TABLE User (
 	UserID INTEGER AUTO_INCREMENT NOT NULL UNIQUE,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
-    Email VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL UNIQUE,
     Street VARCHAR(95) NOT NULL,
     City VARCHAR(35) NOT NULL,
     State CHAR(2) NOT NULL,
     ZipCode CHAR(5) NOT NULL,
-    Country VARCHAR(70),
+    Country VARCHAR(70) NOT NULL,
     PRIMARY KEY (UserID)
 );
 
@@ -48,7 +49,7 @@ CREATE TABLE Buyer (
 	UserID INTEGER NOT NULL UNIQUE,
     LoyaltyStatus BIT NOT NULL, -- BIT uses 1 and 0 to represent True and False respectively
     PRIMARY KEY (UserID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID)
+    CONSTRAINT FK_Buyer_UserID FOREIGN KEY (UserID) REFERENCES User(UserID)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 );
@@ -57,7 +58,7 @@ CREATE TABLE Seller (
 	UserID INTEGER NOT NULL UNIQUE,
     StoreName VARCHAR(50) NOT NULL,
     PRIMARY KEY (UserID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID)
+    CONSTRAINT FK_Seller_UserID FOREIGN KEY (UserID) REFERENCES User(UserID)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 );
